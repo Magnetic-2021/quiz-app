46d2d7ec9d64529b023b66250b7537729601022f
Stephen Booth <stephenboothdev@gmail.com>
2021-03-24 14:55:58 +0000

    Added signup form #14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import "antd/dist/antd.css";
 
+import Signup from "./Components/Signup/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Components/Signup/Signup.js
+++ frontend/src/Components/Signup/Signup.js
@@ -1,6 +1,94 @@
-import input from "antd";
+import { useState } from "react";
+import { Form, Input, Space, Button } from "antd";
+import {
+  PlusCircleOutlined,
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons";
+import "./Signup.css";
 const Signup = () => {
-  return <input></input>;
+  const [formStatus, setFormStatus] = useState("idle");
+  const buttonValues = {
+    idle: {
+      text: "Create Account",
+      loading: false,
+      icon: <PlusCircleOutlined />,
+    },
+    loading: { text: "Creating Account", loading: true, icon: null },
+    success: {
+      text: "Account Created",
+      loading: false,
+      icon: <CheckCircleOutlined />,
+    },
+    failed: {
+      text: "Account Creation Failed",
+      loading: false,
+      icon: <CloseCircleOutlined />,
+    },
+  };
+
+  const onFinish = (values) => {
+    console.log(values);
+    setFormStatus("loading");
+    // post data
+    fetch("http://localhost:5000/user/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setFormStatus("success");
+        } else {
+          setFormStatus("failed");
+        }
+      })
+      .catch((err) => {
+        setFormStatus("failed");
+      });
+  };
+
+  return (
+    <div className="signup-container">
+      <Form name="signup" onFinish={onFinish}>
+        <h1>Create an Account</h1>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input a username." }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email Address"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "A valid email address is required to signup.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{}]}>
+          <Input.Password />
+        </Form.Item>
+        <Form.Item label="Confirm Password" name="confirmPassword" rules={[{}]}>
+          <Input.Password />
+        </Form.Item>
+        <Button
+          htmlType="submit"
+          type="primary"
+          loading={buttonValues[formStatus].loading}
+          icon={buttonValues[formStatus].icon}
+        >
+          {buttonValues[formStatus].text}
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Signup;
