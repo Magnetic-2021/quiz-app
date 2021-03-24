3e0ddf16dc4be761e2c1083cfc4ad2b550a99331
Gemcila <ggemcila@gmail.com>
2021-03-24 15:57:33 +0000

    #12 Basic structure of "Quiz" component

---
+++ frontend/src/Components/Quiz/Quiz.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Quiz = () =>{
+    return(
+        <div className="question-container">
+
+            <div className="question">Question</div>
+
+            <div className="options-container">
+                <div className="option1">A</div>
+                <div className="option2">B</div>
+                <div className="option3">C</div>
+                <div className="option4">D</div>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Quiz;
