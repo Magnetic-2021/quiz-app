085f8e2e357ed6ecc6de830942ee39634d0bc789
Jennifer <jennifacarey@gmail.com>
2021-03-24 17:10:13 +0000

    nav styles

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,29 +1,30 @@
 // import {Link} from "react-router-dom";
 import {useState} from "react";
 import {MenuOutlined, CloseOutlined} from "@ant-design/icons";
+import "./Navbar.css";
 
 const Navbar = () => {
     const [isActive, setIsActive] = useState(true);
 
-    const clickHandler = () => setIsActive(false);
+    const openMenu = () => setIsActive(false);
     const closeMenu = () => setIsActive(true);
 
     return (
         <header>
             {isActive && (
                 <nav className="navbar">
-                    <MenuOutlined className="menu-icon menu-icon-burger" onClick={clickHandler} />
+                    <MenuOutlined className="menu-icon menu-icon-burger" onClick={openMenu} />
                 </nav>
             )}
             {!isActive && (
-                <nav className="navbar">
+                <nav className="navbar navbar-active">
                     <CloseOutlined className="menu-icon menu-icon-close" onClick={closeMenu} />
                     <div className="navbar-links">
-                        <a href="#">Home</a>
-                        <a href="#">Quiz</a>
-                        <a href="#">Leaderboard</a>
-                        <a href="#">About</a>
-                        <a href="#">Sign In / Register</a>
+                        <a href="#" className="navbar-item">Home</a>
+                        <a href="#" className="navbar-item">Quiz</a>
+                        <a href="#" className="navbar-item">Leaderboard</a>
+                        <a href="#" className="navbar-item">About</a>
+                        <a href="#" className="navbar-item">Sign In / Register</a>
                     </div>
                 </nav>
             )}
