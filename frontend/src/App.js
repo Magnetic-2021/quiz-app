4af955fc41a49d6fcdee40586a355bbab19c6df0
Jennifer <jennifacarey@gmail.com>
2021-03-25 09:26:44 +0000

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,44 @@
 import "./App.css";
 import "antd/dist/antd.css";
-import Navbar from "../src/Components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Signup from "./Components/Signup/Signup";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
-  
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            {/* <Route path="/quiz">
+              <Quiz />
+            </Route> */}
+            {/* <Route path="/leaderboard">
+              <Leaderboard />
+            </Route> */}
+            {/* <Route path="*">
+              <NotFound />
+            </Route> */}
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
