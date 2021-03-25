b4b3ebf7349f2aa7c3e66d7da4ab4d0c17bccfaa
Jennifer <jennifacarey@gmail.com>
2021-03-25 11:30:25 +0000

    tidy code

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -7,43 +7,21 @@ import "./Navbar.css";
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
-    // const openMenu = () => setIsActive(false);
-    // const closeMenu = () => setIsActive(true);
-
     const handleToggle = () => setMenuOpen(prev => !prev);
 
     const closeMenu = () => setMenuOpen(false);
 
     return (
-        <header>
-            <nav className="navbar">
-                <button onClick={handleToggle}>{menuOpen ? <CloseOutlined className="navbar-icon" /> : <MenuOutlined className="navbar-icon" /> }</button>
-                <div className={`menuNav ${menuOpen ? "showMenu" : ""}`}>
-                    <Link to="/" className="navbar-item" onClick={closeMenu}>Home</Link>
-                    <Link to="/quiz" className="navbar-item" onClick={closeMenu}>Quiz</Link>
-                    <Link to="/leaderboard" className="navbar-item" onClick={closeMenu}>Leaderboard</Link>
-                    <Link to="/about" className="navbar-item" onClick={closeMenu}>About</Link>
-                    <Link to="/signup" className="navbar-item" onClick={closeMenu}>Sign Up / Register</Link>
-                </div>
-            </nav>
-            {/* {isActive && (
-                <nav className="navbar">
-                    <MenuOutlined className="menu-icon menu-icon-burger" onClick={openMenu} />
-                </nav>
-            )}
-            {!isActive && (
-                <nav className="navbar navbar-active">
-                    <CloseOutlined className="menu-icon menu-icon-close" onClick={closeMenu} />
-                    <div className="navbar-links">
-                        <Link to="/" className="navbar-item">Home</Link>
-                        <Link to="/quiz" className="navbar-item">Quiz</Link>
-                        <Link to="/leaderboard" className="navbar-item">Leaderboard</Link>
-                        <Link to="/about" className="navbar-item">About</Link>
-                        <Link to="/signup" className="navbar-item">Sign Up / Register</Link>
-                    </div>
-                </nav>
-            )} */}
-        </header>
+        <nav className="navbar">
+            <button onClick={handleToggle}>{menuOpen ? <CloseOutlined className="navbar-icon" /> : <MenuOutlined className="navbar-icon" /> }</button>
+            <div className={`menuNav ${menuOpen ? "showMenu" : ""}`}>
+                <Link to="/" className="navbar-item" onClick={closeMenu}>Home</Link>
+                <Link to="/quiz" className="navbar-item" onClick={closeMenu}>Quiz</Link>
+                <Link to="/leaderboard" className="navbar-item" onClick={closeMenu}>Leaderboard</Link>
+                <Link to="/about" className="navbar-item" onClick={closeMenu}>About</Link>
+                <Link to="/signup" className="navbar-item" onClick={closeMenu}>Sign Up / Register</Link>
+            </div>
+        </nav>
     );
 };
  
