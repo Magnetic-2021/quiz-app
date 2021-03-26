17ae47f213ba8fae8f00b78656cc326e73b56ea5
boothscript <40050887+boothscript@users.noreply.github.com>
2021-03-26 12:05:27 +0000

    added get top users scores

--- models/Score.js
+++ models/Score.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const scoreSchema = new mongoose.Schema({
-    username: {type:String, required: true},
-    score: {type:Number, required:true},
-    date: {type:Number, required:true},
-   // userID: {type:mongoose.Schema.Types.ObjectId, ref:'Users', required:true}
+  username: { type: String, required: true },
+  score: { type: Number, required: true },
+  date: { type: Number, required: true },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
 });
 
 const Score = mongoose.model("Scores", scoreSchema);
 
-module.exports = {Score};
+module.exports = { Score };
--- routes/scoreRouter.js
+++ routes/scoreRouter.js
@@ -4,26 +4,42 @@ const { request } = require("express");
 const { Score } = require("../models/Score");
 const router = express.Router();
 
-router.get('/score', (req, res) => {
-
-    Score.find({}, (err, docs) => {
-        res.send(docs)
-    }).sort({ score: -1 }).limit(10);
-
+router.get("/score", (req, res) => {
+  Score.find({}, (err, docs) => {
+    res.send(docs);
+  })
+    .sort({ score: -1 })
+    .limit(10)
+    .catch((err) => {
+      console.log("There was an error", err);
+      res.status(500).send(err);
+    });
 });
 
+router.get("/score/:userID", (req, res) => {
+  Score.find({ userID: req.params.userID }, (err, docs) => {
+    res.send(docs);
+  })
+    .sort({ score: -1 })
+    .limit(10)
+    .catch((err) => {
+      console.log("There was an error", err);
+      res.status(500).send(err);
+    });
+});
 
-router.post('/score', (req, res) => {
-    const newScore = req.body;
-    console.log(newScore);
-    Score.create(newScore).then((obj) => {
-        console.log("New score is created", obj);
-        res.status(200).send({ message: "You're score is saved" });
-    }).catch((err) => {
-        console.log("There was an error", err);
-        res.status(500).send(err);
+router.post("/score", (req, res) => {
+  const newScore = req.body;
+  console.log(newScore);
+  Score.create(newScore)
+    .then((obj) => {
+      console.log("New score is created", obj);
+      res.status(200).send({ message: "You're score is saved" });
+    })
+    .catch((err) => {
+      console.log("There was an error", err);
+      res.status(500).send(err);
     });
 });
 
 module.exports = router;
-
