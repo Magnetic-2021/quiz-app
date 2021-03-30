22de4a535cd68ae37cf4924ba01d1c31dfcc6c3f
boothscript <40050887+boothscript@users.noreply.github.com>
2021-03-30 09:58:58 +0100

    added routes for avatar upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRouter = require("./routes/user");
 
 connect();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/", userRouter);
 app.use("/", scoreRouter);
