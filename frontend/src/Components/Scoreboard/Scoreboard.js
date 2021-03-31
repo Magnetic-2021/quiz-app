ed4f6d403fd1e429586aa430102870dbc930535d
boothscript <40050887+boothscript@users.noreply.github.com>
2021-03-31 09:13:52 +0100

    added table styles and tweaks

--- frontend/src/Components/Scoreboard/Scoreboard.js
+++ frontend/src/Components/Scoreboard/Scoreboard.js
@@ -3,16 +3,18 @@ import { Table, Badge } from "antd";
 import { useHistory } from "react-router-dom";
 import UserAvatar from "../Avatar/Avatar.js";
 import "./Scoreboard.css";
-const Podium = ({score, place}) => {
+const Podium = ({ score, place, color }) => {
   return (
     <div className={`podium-${place}`}>
-    <Badge.Ribbon text={`${place}: ${score.username}`} offset={[0,80]}>
-      <UserAvatar user={{id:score.userID}} size={place === "1st"?124: 96}/>
-    </Badge.Ribbon>
+      <Badge.Ribbon text={`${place}: ${score.username}`} offset={[0, 80]} color={color}>
+        <UserAvatar
+          user={{ id: score.userID }}
+          size={place === "1st" ? 124 : 96}
+        />
+      </Badge.Ribbon>
     </div>
-
-  )
-}
+  );
+};
 const Scoreboard = (props) => {
   console.log("scoreboard mounted");
   const [leaderboardData, setLeaderboardData] = useState([]);
@@ -27,16 +29,23 @@ const Scoreboard = (props) => {
     fetch("http://localhost:5000/score", {})
       .then((res) => res.json())
       .then((data) => {
-        setLeaderboardData(data);
+        setLeaderboardData(
+          data.map((score, index) => {
+            return {
+              ...score,
+              date: new Date(score.date).toLocaleDateString("en-GB"),
+              index: index +1,
+              key: score._id,
+            };
+          })
+        );
         setTableState("loaded");
       })
       .catch(console.log("unable to fetch scores from server"));
   }, []);
 
   const columns = [
-    {title: "#",
-    dataIndex: "index",
-    key:"index"},
+    { title: "#", dataIndex: "index", key: "index" },
     {
       title: "User",
       dataIndex: "username",
@@ -53,21 +62,23 @@ const Scoreboard = (props) => {
       key: "date",
     },
   ];
-  const places = ["1st", "2nd", "3rd"]
+  const places = ["1st", "2nd", "3rd"];
+  const medals = ["#AF9500", "#B4B4B4", "#6A3805"]
   return (
     <div className="scoreboard-container">
-      <h1 className="leadHeader">LEADERBOARD</h1>
       <div className="podium">
-        {leaderboardData?.slice(0,3).map((score, index) => {
-          const place = places[index]
-          return (
-            <Podium key={place} score={score} place={places[index]} />
-          )
-})}
+        {leaderboardData?.slice(0, 3).map((score, index) => {
+          const place = places[index];
+          return <Podium key={place} score={score} place={places[index]} color={medals[index]} />;
+        })}
       </div>
       <div className="tableScore">
         {tableState === "loaded" ? (
-          <Table dataSource={leaderboardData} columns={columns} />
+          <Table
+            dataSource={leaderboardData}
+            columns={columns}
+            pagination={false}
+          />
         ) : (
           <div className="loading-container">
             <div className="loader-circle"></div>
@@ -79,6 +90,5 @@ const Scoreboard = (props) => {
     </div>
   );
 };
-
 
 export default Scoreboard;
