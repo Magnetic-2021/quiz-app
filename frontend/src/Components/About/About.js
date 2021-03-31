5fd7481bc77ff7d7944200fda22e4afaa0121702
Jennifer <jennifacarey@gmail.com>
2021-03-31 09:06:23 +0100

    update content

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -5,16 +5,18 @@ const About = () => {
     <div className="container">
       <h1>About</h1>
       <p className="appAbout">
-        Knowledge Bomb is a general knowledge quiz designed to test the users
-        skills against the clock. You have to solve the quiz before the timer
-        ends and the bomb explodes...!
+        Knowledge Bomb is a general knowledge quiz designed to test the user's
+        skills against the clock. Answer 10 questions before the timer
+        ends and the bomb explodes.
       </p>
-      <p className="appAbout">
-        There are three levels to test yourself against the fuse, easy, medium
-        and hard.
+      <p className="app-about">
+        The questions could be either multiple choice or true or false. The more answers you get right, the more time you have and the higher your score. But run out of time and it is game over!
       </p>
 
-      <h2 className="teamHeader">Meet the team</h2>
+      <h2 className="team-header">Meet the team</h2>
+      <p className="app-about">
+        Knowledge Bomb was created and built by Steve, Gemcila, Jennifer and Phil as our final project for Code Nation's 12-week immersive coding bootcamp. <a href="https://wearecodenation.com/event/master-coding/" target="_blank" rel="noreferrer">Click here</a> for more information or give our GitHub profiles a visit below.
+      </p>
       <div className="team-container">
         <div className="team-member">
           <a href="https://github.com/boothscript" className="team-link">
