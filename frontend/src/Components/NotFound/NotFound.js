65ed869eda384cb24d932c7ee70ee790e2b95fa2
Gemcila <ggemcila@gmail.com>
2021-03-31 11:10:49 +0100

    #109 Styling 404page

--- frontend/src/Components/NotFound/NotFound.js
+++ frontend/src/Components/NotFound/NotFound.js
@@ -1,13 +1,21 @@
 import {Link} from "react-router-dom";
+import "./NotFound.css";
+import notfoundLogo from "../../images/notfoundLogo.svg";
 
 const NotFound = () => {
     return ( 
         <div className="not-found-page">
-            <h2>Oops! Sorry.</h2>
-            <p>That page cannot be found.</p>
-            <Link to="/">Back to homepage...</Link>
+            <div className="notfound-container"> 
+            <h2 className="notFound-title">Oops! Sorry.</h2>
+            <p className="notFound-subtitle">That page cannot be found.</p>
+            <Link to="/" className="backtohome">Back to homepage...</Link>
+            </div>
+
+            <div className="notfoundlogo-container">
+            <img src={notfoundLogo} alt="notfoundlogo" width="230px"></img>
+            </div>
         </div>
     );
 };
- 
+
 export default NotFound;
