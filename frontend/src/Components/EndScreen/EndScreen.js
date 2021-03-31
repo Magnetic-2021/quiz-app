95c0de182c9016109fdd9ad4462acc62921f5377
Gemcila <ggemcila@gmail.com>
2021-03-31 15:59:28 +0100

    #89 Structure of divs

--- frontend/src/Components/EndScreen/EndScreen.js
+++ frontend/src/Components/EndScreen/EndScreen.js
@@ -1,7 +1,8 @@
 import "./EndScreen.css";
 import Avatar from "../Avatar/Avatar";
 import { useHistory } from 'react-router-dom';
-
+import trophy from "../../images/trophylogo.svg";
+import explotion from "../../images/explotionlogo.svg";
 
 const EndScreen = (props) => {
   const history = useHistory();
@@ -10,8 +11,25 @@ const EndScreen = (props) => {
       <div className="EndScreen">
 
         <div className="endscreen-title-container">
+          <div> 
+          <h1>Congratulations!</h1>
+          </div>
+
+          <div> 
+          <img src={trophy}/>
+          </div> 
+          
+          <div> 
           <h3 className="endscreenTitle">Your score</h3>
-          <h1 className="endscreenScore">{props.score}</h1>
+          </div> 
+
+          <div>   
+          <h2 className="endscreenScore">300</h2>
+          </div> 
+          
+          <div>
+          <img src={explotion}/>
+          </div>
         </div>
 
         <div className="endscreen-avatar-container">
