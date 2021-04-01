a1763be6897a53b213ab96e2e33cad391a23f0da
boothscript <40050887+boothscript@users.noreply.github.com>
2021-04-01 08:10:43 +0100

    added conditional home page buttton logic

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -5,43 +5,65 @@ import clocklogo from "../../images/clocklogo.svg";
 import imageex from "../../images/imageex.svg";
 import hero from "../../images/hero.svg";
 
-const Home = () => {
+const Home = (props) => {
   const history = useHistory();
 
   return (
     <div className="Home">
+      <div className="container1">
+        <div className="hometitle-container">
+          <h1 className="hometitle">CAN YOU BEAT THE BOMB?</h1>
+          <h3 className="homesubtitle">
+            Small and cool description of the game
+          </h3>
+        </div>
 
-     <div className="container1"> 
-      <div className="hometitle-container">
-        <h1 className="hometitle">CAN YOU BEAT THE BOMB?</h1>
-        <h3 className="homesubtitle">Small and cool description of the game</h3>
+        <div className="home-buttonsContainer">
+          {!props.user ? (
+            <div className="buttoncontainer1">
+              <button
+                onClick={() => history.push("/signup")}
+                className="btn homebtn getstarted"
+              >
+                GET STARTED
+              </button>
+              <button
+                onClick={() => history.push("/Howtoplay")}
+                className="btn homebtn white"
+              >
+                HOW TO PLAY
+              </button>
+            </div>
+          ) : (
+            <div className="buttoncontainer2">
+              <button
+                onClick={() => history.push("/quiz")}
+                className="btn homebtn white"
+              >
+                PLAY NOW
+              </button>
+              <button
+                onClick={() => history.push("/leaderboard")}
+                className="btn homebtn white"
+              >
+                LEADERBOARD
+              </button>
+            </div>
+          )}
+        </div>
       </div>
 
-    
-      <div className="home-buttonsContainer"> 
-      
-      <div className="buttoncontainer1"> 
-      <button onClick={() => history.push("/signup")} className="btn homebtn orange">GET STARTED</button>
-      <button onClick={() => history.push("/Howtoplay")} className="btn homebtn white">HOW TO PLAY</button>
+      <div className="imagecontainer">
+        <img src={imageex} className="imageex" alt="imageex" width="400" />
+        <img src={hero} className="herologo" alt="imageex" width="350" />
+        <img
+          src={clocklogo}
+          className="clocklogo"
+          alt="clocklogo"
+          width="200"
+        />
       </div>
-
-      <div className="buttoncontainer2"> 
-      <button onClick={() => history.push("/login")} className="btn homebtn orange">PLAY NOW</button>
-      <button onClick={() => history.push("/leaderboard")} className="btn homebtn white">LEADERBOARD</button>
-      </div>
-      
-      </div>
-      </div>
-
-      <div className="imagecontainer"> 
-        <img src={imageex} className="imageex" alt="imageex" width="400"/>        
-        <img src={hero} className="herologo" alt="imageex"  width="350"/>
-        <img src={clocklogo} className="clocklogo" alt="clocklogo" width="200"/>
-      </div>
-
     </div>
   );
 };
 export default Home;
-
- 
