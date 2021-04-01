32a1e6330752dda4c6152456416801df3a542acf
Jennifer <jennifacarey@gmail.com>
2021-04-01 10:59:27 +0100

    heroku deployment

--- index.js
+++ index.js
@@ -11,14 +11,26 @@ connect();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  });
+}
+
 app.use("/", userRouter);
 app.use("/", scoreRouter);
 app.use("/", questionRouter);
 
-app.listen(5000, (err) => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
   if (err) {
     console.log("error", err);
   } else {
-    console.log("listening on port 5000");
+    console.log("Online");
   }
 });
